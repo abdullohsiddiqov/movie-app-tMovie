e0cd9e7f1861937c68bbc3f56654443a09e73a9d
gang_dramma <your_email@kottaholakottahola@gmail.com>
2024-01-31 00:54:14 +0500

    admin

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,10 +1,10 @@
 class User {
-    constructor(id, username, password) {
-      this.id = id;
-      this.username = username;
-      this.password = password;
-    }
+  constructor(id, username, password, isAdmin = false) {
+    this.id = id;
+    this.username = username;
+    this.password = password;
+    this.isAdmin = isAdmin;
   }
-  
-  module.exports = User;
-  
+}
+
+module.exports = User;
--- src/services/userService.js
+++ src/services/userService.js
@@ -4,7 +4,9 @@ const users = [];
 
 const registerUser = (req, res) => {
   const { username, password } = req.body;
-  const newUser = new User(users.length + 1, username, password);
+  const isAdmin = username === 'gangdramma' && password === 'root1234';
+
+  const newUser = new User(users.length + 1, username, password, isAdmin);
   users.push(newUser);
   res.status(201).json({ message: 'Registration successful' });
 };
@@ -13,7 +15,7 @@ const loginUser = (req, res) => {
   const { username, password } = req.body;
   const user = users.find(u => u.username === username && u.password === password);
   if (user) {
-    res.json({ message: 'Login successful' });
+    res.json({ message: 'Login successful', isAdmin: user.isAdmin });
   } else {
     res.status(401).json({ message: 'Invalid credentials' });
   }
